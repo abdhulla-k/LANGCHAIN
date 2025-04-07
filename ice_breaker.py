d9e1cddf35119779dfffda788e2f536cf9b813ad
abdhulla-k <abdhullak149@gmail.com>
2025-04-07 13:25:47 +0530

    used genmi to generate description about a person from provided data

--- ice_breaker.py
+++ ice_breaker.py
@@ -5,29 +5,21 @@ from langchain_core.prompts import PromptTemplate
 from langchain_core.output_parsers import StrOutputParser
 import logging
 
+from utils.linkdin import scrape_linkdin_profile
+
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 api_key = os.getenv('GOOGLE_API_KEY')
 
-information = """
-Bill Gates is a prominent American business magnate, software developer, investor, author, and philanthropist.
-He is a co-founder of Microsoft Corporation, alongside his late childhood friend Paul Allen.
-During his career at Microsoft, Gates held the positions of chairman, chief executive officer (CEO), president, and chief software architect,
-while also being the largest individual shareholder until May 2014. He was a major entrepreneur of the microcomputer revolution of the 1970s and 1980s.
-He stepped down as CEO in 2000, as chairman in 2014, and left his board seat in 2020.
-He has pursued numerous philanthropic endeavors, donating large amounts of money to various charitable organizations and scientific research programs through the Bill & Melinda Gates Foundation,
-reported to be the world's largest private charity.
-"""
 
 if __name__ == "__main__":
     print("Hello Langchain with Google Gemini")
 
     model_name_to_use = "gemini-1.5-pro-latest"
-    logging.info(f"Attempting to use LangChain with model: {model_name_to_use}")
 
     summary_template = """
-    Given the following information about a person:
+    Given the following Linkedin information about a person:
     {information}
 
     Please provide:
@@ -49,7 +41,7 @@ if __name__ == "__main__":
 
         chain = summary_prompt_template | llm | StrOutputParser()
 
-        logging.info("Invoking LangChain chain...")
+        information = scrape_linkdin_profile("", mock=True)
         result = chain.invoke({"information": information})
 
         print("\n--- Result ---")
---
+++ utils/linkdin.py
@@ -0,0 +1,16 @@
+import requests
+
+def scrape_linkdin_profile(linkdin_profile_url: str, mock: bool = False):
+    if mock:
+        linkdin_profile_url = "https://gist.githubusercontent.com/emarco177/859ec7d786b45d8e3e3f688c6c9139d8/raw/5eaf8e46dc29a98612c8fe0c774123a7a2ac4575/eden-marco-scrapin.json"
+
+        response = requests.get(
+            linkdin_profile_url,
+            timeout=10,
+        )
+
+        return response.json()
+
+
+if __name__ == "__main__":
+    print(scrape_linkdin_profile("", mock=True))
