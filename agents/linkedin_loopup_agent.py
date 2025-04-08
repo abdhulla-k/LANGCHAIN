2124561b00710ad090b347a6d697720236ecaed6
abdhulla-k <abdhullak149@gmail.com>
2025-04-09 01:40:17 +0530

    created a search agent to gquery linkedin url  with prompt

---
+++ agents/linkedin_loopup_agent.py
@@ -0,0 +1,54 @@
+import os
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate
+from langchain_core.tools import Tool
+from langchain.agents import create_react_agent, AgentExecutor
+from langchain import hub
+
+from tools.tools import get_profile_url_tavily
+
+api_key = os.getenv('GOOGLE_API_KEY')
+model_name_to_use = os.getenv('MODEL_NAME_TO_USE')
+
+
+def lookup(name: str) -> str:
+    llm = ChatGoogleGenerativeAI(
+        model=model_name_to_use,
+        google_api_key=api_key,
+        temperature=0.1,
+    )
+
+    template = """
+    given the full name {name_of_person} I wan to get it me a link to their Linkedin profile page.
+    Your anser should contain only a URL
+    """
+
+    prompt_template = PromptTemplate(
+        input_variables=["name_of_person"],
+        template=template
+    )
+
+    tools_for_agent = [
+        Tool(
+            name="Crawl Google for Linkedin profile page",
+            func=get_profile_url_tavily,
+            description="useful for when you need to get the Linkedin profile page URL for a person"
+        )
+    ]
+
+    react_prompt = hub.pull("hwchase17/react")
+    agent = create_react_agent(llm=llm, tools=tools_for_agent, prompt=react_prompt)
+    agent_executor = AgentExecutor(agent=agent, tools=tools_for_agent, verbose=True)
+
+    result = agent_executor.invoke(
+        input= { "input": prompt_template.format_prompt(name_of_person=name)}
+    )
+
+    linked_profile_url = result["output"]
+    return linked_profile_url
+
+
+
+if __name__ == "__main__":
+    linkedin_url = lookup(name = "Eden Marco")
--- ice_breaker.py
+++ ice_breaker.py
@@ -6,6 +6,7 @@ from langchain_core.output_parsers import StrOutputParser
 import logging
 
 from utils.linkdin import scrape_linkdin_profile
+from agents.linkedin_loopup_agent import lookup
 
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
@@ -16,7 +17,7 @@ api_key = os.getenv('GOOGLE_API_KEY')
 if __name__ == "__main__":
     print("Hello Langchain with Google Gemini")
 
-    model_name_to_use = "gemini-1.5-pro-latest"
+    model_name_to_use = os.getenv('MODEL_NAME_TO_USE')
 
     summary_template = """
     Given the following Linkedin information about a person:
@@ -46,7 +47,11 @@ if __name__ == "__main__":
 
         print("\n--- Result ---")
         print(result)
-        print("--------------")
+        print("\n--------------")
+
+        user = input("\nEnter user name and basic details to search the profile: ")
+        linkedin_url = lookup(name=user)
+        print(linkedin_url)
 
     except Exception as e:
         logging.error(f"An error occurred during LangChain execution: {e}", exc_info=True)
