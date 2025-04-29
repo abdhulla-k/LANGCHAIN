77ef024fdf553c6047069ad75041e3ceed45fa09
abdhulla-k <abdhullak149@gmail.com>
2025-04-29 20:18:21 +0530

    leaned creating rag chain without using langchian specific function

--- rag-intro/main.py
+++ rag-intro/main.py
@@ -9,7 +9,13 @@ from langchain import hub
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains.retrieval import create_retrieval_chain
 
+from langchain_core.runnables import RunnablePassthrough
+
 load_dotenv()
+
+# Function to use in custom rag implimentation
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 if __name__ == "__main__":
     print("retrieving..")
@@ -44,3 +50,28 @@ if __name__ == "__main__":
     print("\n\n\n\n\n")
     print(result)
     
+
+    ##
+    ## Check waht happening behind the sean of the langchain functon through writing it.
+    ##
+    template = """Use the following pieces of context to answer the question at the end.
+    If you don't know the answer, just say that you don't know, don't try to make up an answer.
+    Use three sentences maximum and keep the answer as concise as possible.
+    Always say "thanks for asking!" at the end of the answer.
+
+    {context}
+
+    Question: {Question}
+
+    Helpful Answer:"""
+
+    custom_rag_prompt = PromptTemplate.from_template(template)
+    rag_chain = (
+        { "context": vectorstore.as_retriever() | format_docs, "Question": RunnablePassthrough()}
+        | custom_rag_prompt
+        | llm
+    )
+
+    custom_res = rag_chain.invoke(query)
+    print("\n\n\n\n\n")
+    print(custom_res)
